e5d4dabf12428f2ba20ed829f5ad8d6f28e9516b
Austin Estrella <ausfrest@espol.edu.ec>
2023-10-28 19:24:39 -0500

    Revert "ButtonEvent"
    This reverts commit 726fde2d774171c4f98b9ee5377a04945bffe33b.

--- src/gui/ButtonEvent.java
+++ src/gui/ButtonEvent.java
@@ -1,8 +1,4 @@
 package gui;
-import java.awt.Color;
-
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import java.awt.Color;
 
@@ -11,14 +7,6 @@ import javax.swing.JPanel;
 
 
 public class ButtonEvent {
-	public static void updateBackground(JPanel jp,Color c) {
-		jp.setBackground(c);
-	}
-	public static void setLabelContent(JLabel l,String s) {
-		l.setText(s);	
-	}
-
-	
 	public static void updateBackground(JPanel jp,Color c) {
 		jp.setBackground(c);
 	}
